a0d3b533229cf98398bf108efa18343daaf6cf75
Davide Valentino <daval302@gmail.com>
2019-06-24 12:40:52 +0100

    get 2 bytes ID fro json configurationn file

--- example/clientA/main.go
+++ example/clientA/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Conf struct {
+	Name string
+	ID   string
+}
+
+var conf Conf
+
 func main() {
 
+	// Load configuration json file
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Load configuration file")
+	defer configFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(configFile)
+
+	json.Unmarshal(byteValue, &conf)
+
+	// get 2 bytes json as ID
+	clientID := []byte(conf.ID)
+
+	//fmt.Printf("client : %#x\n", clientID)
+	fmt.Print(clientID)
 }
